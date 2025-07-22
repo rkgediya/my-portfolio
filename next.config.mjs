b9b8e7f44f6552b3499708ca6ba22141ba6269b5
Ronak Gediya <gediyaronak200@gmail.com>
2025-07-22 11:26:33 +0530

    Update next config file - 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,15 @@
-// next.config.js
-import nextMDX from '@next/mdx';
+import { withMDX } from '@next/mdx';
 
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    // Any mdx options here (optional)
-  },
-});
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Required for GitHub Pages
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  transpilePackages: ['next-mdx-remote'],
   sassOptions: {
     compiler: 'modern',
     silenceDeprecations: ['legacy-js-api'],
   },
 };
 
-export default withMDX(nextConfig);
+export default withMDX({
+  extension: /\.mdx?$/,
+})(nextConfig);
+
